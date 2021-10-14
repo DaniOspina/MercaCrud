8a9a693065fb85d6c90c405b245346ec1ce4717e
DaniOspina <danielaos94@gmail.com>
2021-10-13 21:47:13 -0500

    fix routes

--- crud-mercatodo/src/App.js
+++ crud-mercatodo/src/App.js
@@ -1,10 +1,22 @@
-import Routes from './routes/Routes';
+import React from 'react';
+import {Switch, Route} from 'react-router-dom'
+import User from './components/userview/User';
+import Admin from './pages/admin/Admin';
+import Login from './pages/login/Login';
 
 function App() {
   return (
-    <div>
-      <Routes/>
-    </div>
+    <>
+      <Switch>
+            <Route exact path="/" > <Login/> </Route>
+      </Switch>
+      <Switch>
+            <Route exact path="/admin" > <Admin/> </Route>
+      </Switch>
+      <Switch>
+            <Route exact path="/user" > <User/> </Route>
+      </Switch>
+    </>
   );
 }
 
